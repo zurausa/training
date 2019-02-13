1bfa0c447deffdd72b83f7f83d9695437b385e50
TakuyaIto <zurausa@zurausa-mac.hitronhub.home>
2019-02-12 22:53:33 -0800

    change calc

--- S015.py
+++ S015.py
@@ -5,15 +5,10 @@ k, s, t = map(int, sys.stdin.readline().split())
 
 
 def calc_len(x):
-    c1 = 0
-    c2 = 0
-    if x - 1 > 0:
-        c1 = int(math.pow(2, (x - 1)) * 3)
-    if x - 2 > 0:
-        c2 = int(math.pow(2, (x - 2)) - 1) * 6
-        if c2 < 0:
-            c2 = 0
-    return c1 + c2 + 3
+    le = 0
+    for _ in range(x):
+        le = le * 2 + 3
+    return le
 
 
 def get_range_text(k, s, t):
@@ -47,16 +42,4 @@ def get_range_text(k, s, t):
     return res
 
 
-print(get_range_text(k, s-1, t-1))
-
-# st_dict = {}
-# st_dict[1] = "ABC"
-
-# def create(x):
-#     if x in st_dict:
-#         return st_dict[x]
-#     else:
-#         st_dict[x] = f"A{create(x-1)}B{create(x-1)}C"
-#         return st_dict[x]
-
-# print(create(k)[s-1:t])
+print(get_range_text(k, s - 1, t - 1))
